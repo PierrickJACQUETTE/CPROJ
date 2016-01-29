71c1ed4ba93f3ee4ffb5a324ecaacfcce9d60d0b
PierrickJACQUETTE <pierrick.jacquette@gmail.com>
2016-01-29 19:10:27 +0100

    la bonne version de la bibliotheque libsdl avec le bon fichier test

--- testSdl.c
+++ testSdl.c
@@ -1,29 +1,38 @@
-#include <stdlib.h>
+#include "SDL.h"
 #include <stdio.h>
-#include <SDL/SDL.h>
- 
-void pause();
 
-int main(int argc, char *argv[])
-{
-    SDL_Init(SDL_INIT_VIDEO); // Initialisation de la SDL
-    SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // Ouverture de la fenêtre
-    pause(); // Mise en pause du programme
-    SDL_Quit(); // Arrêt de la SDL
-    return EXIT_SUCCESS; // Fermeture du programme
-}
+int main(int argc, char* argv[]) {
 
-void pause()
-{
-	int continuer = 1;
-    SDL_Event event;
-    while (continuer)
-    {
-        SDL_WaitEvent(&event);
-        switch(event.type)
-        {
-            case SDL_QUIT:
-                continuer = 0;
-        }
+    SDL_Window *window;                    // Declare a pointer
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "An SDL2 window",                  // window title
+        SDL_WINDOWPOS_UNDEFINED,           // initial x position
+        SDL_WINDOWPOS_UNDEFINED,           // initial y position
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
     }
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+
+    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
+    return 0;
 }
+
