7a4d0a2df3ffa722b17e5320095cb58b85521b0d
PierrickJACQUETTE <pierrick.jacquette@gmail.com>
2016-02-08 22:23:52 +0100

    Voila des nouveaux test graphique un peu plus poussé et permettant de dessiner des polygones remplies

---
+++ testsdl/test_opengl2.c
@@ -0,0 +1,103 @@
+#include "SDL.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+
+
+/*
+gcc fichier.c -o tutorial1 -lGL -lGLU `sdl2-config --cflags --libs`
+)*/
+#include <stdlib.h>
+#include <GL/glu.h>
+
+
+int Display_SetViewport( int width, int height )
+{
+    /* Height / width ration */
+    GLfloat ratio;
+
+    /* Protect against a divide by zero */
+    if ( height == 0 ) {
+        height = 1;
+    }
+
+    ratio = ( GLfloat )width / ( GLfloat )height;
+
+    /* Setup our viewport. */
+    glViewport( 0, 0, ( GLsizei )width, ( GLsizei )height );
+
+    /* change to the projection matrix and set our viewing volume. */
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity( );
+
+    /* Set our perspective */
+    gluPerspective( 45.0f, ratio, 0.1f, 100.0f );
+
+    /* Make sure we're chaning the model view and not the projection */
+    glMatrixMode( GL_MODELVIEW );
+
+    /* Reset The View */
+    glLoadIdentity( );
+
+    return 1;
+}
+
+
+void Display_Render( SDL_Renderer* displayRenderer)
+{
+    /* Set the background black */
+    glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+    /* Clear The Screen And The Depth Buffer */
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+    /* Move Left 1.5 Units And Into The Screen 6.0 */
+    glLoadIdentity();
+    glTranslatef( -1.5f, 0.0f, -6.0f );
+
+    glBegin( GL_TRIANGLES );            /* Drawing Using Triangles */
+      glVertex3f(  0.0f,  1.0f, 0.0f ); /* Top */
+      glVertex3f( -1.0f, -1.0f, 0.0f ); /* Bottom Left */
+      glVertex3f(  1.0f, -1.0f, 0.0f ); /* Bottom Right */
+    glEnd( );                           /* Finished Drawing The Triangle */
+
+    /* Move Right 3 Units */
+    glTranslatef( 3.0f, 0.0f, 0.0f );
+
+	glColor3ub(0, 0, 255); // on demande du bleu
+	glBegin(GL_POLYGON); // debut du dessin
+	  glVertex2f(-.5,  -1); // envoi du premier sommet
+	  glVertex2f( -1,   0); // puis le second
+	  glVertex2f(-.5,   1); // ect
+	  glVertex2f( .5,   1);
+	  glVertex2f(  1,   0);
+	  glVertex2f( .5,  -1); // jusqu'au dernier
+	glEnd(); // fin du dessin                         /* Done Drawing The Quad */
+    
+    SDL_RenderPresent(displayRenderer);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* displayWindow;
+    SDL_Renderer* displayRenderer;
+    SDL_RendererInfo displayRendererInfo;
+    SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_OPENGL, &displayWindow, &displayRenderer);
+    SDL_GetRendererInfo(displayRenderer, &displayRendererInfo);
+    /*TODO: Check that we have OpenGL */
+    if ((displayRendererInfo.flags & SDL_RENDERER_ACCELERATED) == 0 || 
+        (displayRendererInfo.flags & SDL_RENDERER_TARGETTEXTURE) == 0) {
+        /*TODO: Handle this. We have no render surface and not accelerated. */
+    }
+
+    Display_SetViewport(800, 600);
+    Display_Render(displayRenderer);
+    SDL_Delay(5000);
+    SDL_Quit();
+    
+    return 0;
+}
