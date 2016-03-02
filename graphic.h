ce493da00e9102f95db3fc9b7213451775b7c6bb
Hafca <hafca.tirichine@gmail.com>
2016-03-02 18:07:37 +0100

    adding documentation to graphic.h

--- graphic.h
+++ graphic.h
@@ -6,8 +6,8 @@
  *
  */
 
-#ifndef __CORE_H__
-#define __CORE_H__
+#ifndef __GRAPHIC_H__
+#define __GRAPHIC_H__
 
 #include <stdio.h>
 #include <string.h>
@@ -18,14 +18,66 @@
 #include <GL/glu.h>
 #include "Core.h"
 
-#endif
-
+/**
+* Fonction that draws a point in the window
+*
+* @param  int xClick the coordinate of the abscisse of the point
+* @param  int yClick the coordinate of the ordonate of the point
+* @return  nothing it just draws the point
+*/
 void affichage(int xClick,int yClick);
+
+/**
+* Fonction that puts the abscisse's value to the window scale
+* 
+* @param  float x the abscisse value of the point
+* @param  Bounds* bounds the window's bounds
+* @return  value of the abscisse put to scale
+*/
 int miseAEchelleAbs(float x, Bounds* bounds);
+
+/**
+* Fonction that puts the ordonate's value to the window scale 
+*
+* @param  float x the ordonate value of the point
+* @param  Bounds* bounds the window's bounds
+* @return  value of the ordonate put to scale
+*/
 int miseAEchelleOrd(float x, Bounds* bounds);
+
+/**
+* Fonction that browse the avl tree and displays the nodes in the window by calling the affichage fonction
+*
+* @param  Avl **a is the pointer to the avl of node we need to browse
+* @param  Bounds* bounds the window's bounds
+* @return  value of the abscisse put to scale
+*/
 void parcoursAvl(Avl **a, Bounds* bounds);
+
+/**
+* Fonction that permit the display
+* While we haven't closed the window it is still on the screen
+* When we do close it, it displays a message saying we closed it
+* This event is handeled by a loop : while the statut is "CONTINUE" we keep the window to the screen
+* Otherwise, we close it and change the statut to "QUIT"
+*
+* @param  Avl **a is the pointer to the avl of node we need to browse
+* @param  Bounds* bounds the window's bounds
+* @return  value of the abscisse put to scale
+*/
 void evenement();
+
+/**
+* Fonction that creates a window with the right scales, browse the avl tree by calling the "parcoursAvl" fonction 
+* and displays it to the screen by calling the "evenement" fonction
+*
+* @param  Avl **a is the pointer to the avl of node we need to browse
+* @param  Bounds* bounds the window's bounds
+* @return  value of the abscisse put to scale
+*/
 void printMap(Map* map);
+
+#endif /* __GRAPHIC_H__ */
 
 
 
