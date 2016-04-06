2a574ba7b4bfdcf0014991859621761f469da5af
Hafca <hafca.tirichine@gmail.com>
2016-04-06 19:05:14 +0200

    Cleaning the repository
    - Delete the map exemple

--- line.h
+++ line.h
@@ -2,7 +2,9 @@
 * \file line.h
 * \brief This file displays polygons on the map depending on what is stored in the different structures
 * \author Isabelle MARINO Pierrick JACQUETTE Hafça TIRICHINE
-* \date 27 mars 2016
+* \date 6 April 2016
+*
+*
 */
 
 #ifndef __LINE_H__
@@ -11,13 +13,13 @@
 #include "conversionElements.h"
 #include "Avl.h"
 
+
 /**
 * \fn  parcoursListWay(Map *map);
 * \brief Courses way the list
 *
 * \param map This represents all of our data
 * \param renderer That's where we draw
-
 */
 void parcoursListWay(Map *map);
 
