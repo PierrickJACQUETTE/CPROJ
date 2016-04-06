a3cedf80dce5dd4323f2989b9a23c34742c3cf2f
Hafca <hafca.tirichine@gmail.com>
2016-04-06 17:53:58 +0200

    Factorization of the code (1)
    Delete of conversionGraphic.*

--- conversionGraphic.c
+++
@@ -1,9 +0,0 @@
-#include "conversionGraphic.h"
-
-float miseAEchelleX(float x, float y,int size){
-  return x*size/y;
-}
-
-float miseAEchelleY(float x, float y,int size){
-  return size-(x*size/y);
-}
