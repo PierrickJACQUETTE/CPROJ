2f3e3d15e879a217c9e59ac49906e06ee222bda1
PierrickJACQUETTE <pierrick.jacquette@gmail.com>
2016-04-18 11:20:09 +0200

    mise a jour

--- testsdl/dessin1.c
+++ testsdl/dessin1.c
@@ -8,16 +8,6 @@ int windows_Width;
 /*
 gcc -Wall dessin1.c -lSDL2_gfx `sdl2-config --cflags --libs` -lm
 
-Zoom 
-	avant fois 2
-	arriere jusqua 1
-
-Bouge dans la limite de la moitie de la carte en dehors de la zone de vue
-	au clavier 4 directions de 10 en 10
-	a la souris, suivant la souris
-
-Fenetre cree en fonction de la taille du dessin
-
 */
 
 SDL_Window *window = NULL;
@@ -25,22 +15,11 @@ SDL_Renderer *renderer = NULL;
 enum status {QUIT, CONTINUE};
 enum status status;
 
-//new 3
-float zoom;
-int deplacX;
-int deplacY;
-const int PASCLAVIER = 10;
-int pasSourisX;
-int pasSourisY;
-int deplacZX;
-int deplacZY;
-int clicker =0;//1 on deplace
 
 typedef struct{
 	float x;
 	float y;
 }Coordinate;
-
 
 
 void dessin();
@@ -49,7 +28,7 @@ int colorBackground();
 
 //new 1
 void sizeWindows(){
-	int minX = 0; // Bounds 
+	int minX = 0; // Bounds
 	int maxX = 100;
 	int minY = 0;
 	int maxY = 100;
@@ -65,7 +44,7 @@ void sizeWindows(){
 		windows_Width = diffX*QX;
 		windows_Height = diffY*QX;
 	}
-	
+
 }
 
 int init_SDL() {
@@ -97,15 +76,6 @@ void close_SDL() {
 }
 
 //new 1
-void dessine(){
-		colorBackground();
-		SDL_RenderClear(renderer);
-		dessin();
-	    SDL_RenderPresent(renderer);
-}
-
-
-//new 1
 int colorBackground(){
 	if(SDL_SetRenderDrawColor(renderer,0xFF,0xFF,0xFF,0xFF) < 0) {
         printf("Renderer color could not be set! SDL Error: %s\n",SDL_GetError());
@@ -114,70 +84,10 @@ int colorBackground(){
 	return 1;
 }
 
-//new 3
-void windowEvent(SDL_Event e){
-	switch (e.window.event) {
-		case SDL_WINDOWEVENT_RESIZED:
-			windows_Width = e.window.data1;
-             windows_Height = e.window.data2;
-			dessine();
-			break;
-			default:
-				break;
-	}
-}
-
-//new 3
-int calculateZoom(int refX, int refY,int decalage,int flag){ //flag vaut 0 qd + 1 qd -
-	if(flag == 0){
-		zoom *= 2;
-		decalage=(decalage*2)+0.5;
-	}
-	else if(flag == 1){
-		zoom /= 2;
-		decalage=(decalage-0.5)/2;
-	}
-	deplacZX = -decalage*2*refX;
-	deplacZY = -decalage*2*refY;
-	if(flag == 0){
-		deplacX*=2;
-		deplacY*=2;
-	}
-	else{
-		deplacX/=2;
-		deplacY/=2;
-	}
-	dessine();
-	return decalage;
-}
 
 //new 3
 int clavier(SDL_Event e,int i){
-	switch(e.key.keysym.sym){   
-		case SDLK_p:
-			i = calculateZoom(windows_Width/2,windows_Height/2,i,0);
-	        break;
-		case SDLK_m:
-			if(zoom >= 2){
-				i = calculateZoom(windows_Width/2,windows_Height/2,i,1);
-			}
-			break;
-		case SDLK_UP:	
-			deplacY += PASCLAVIER;
-			dessine();
-			break;
-		case SDLK_DOWN:	
-			deplacY -= PASCLAVIER;
-			dessine();
-			break;
-		case SDLK_RIGHT:	
-			deplacX -= PASCLAVIER;
-			dessine();
-			break;
-		case SDLK_LEFT:	
-			deplacX += PASCLAVIER;
-			dessine();
-			break;
+	switch(e.key.keysym.sym){
 		case SDLK_q :
     		printf("fermeture de la fenetre.\n");
     		status = QUIT;
@@ -190,43 +100,11 @@ int clavier(SDL_Event e,int i){
 
 //new 3
 void evenement(){
-	int sourisX = 0;
-	int sourisY = 0;
 	float i = 0;
   do {
     SDL_Event e;
     if (SDL_PollEvent(&e)) { // detecte si il y a un even
       switch (e.type) {
-		case SDL_WINDOWEVENT :
-			windowEvent(e);
-			break;
-		case SDL_MOUSEBUTTONDOWN:
-			pasSourisX = e.motion.x;
-			pasSourisY = e.motion.y;
-			clicker = 1;
-			break;
-		case SDL_MOUSEBUTTONUP:
-			clicker = 0;
-			break;
-		case SDL_MOUSEMOTION :
-			sourisX = e.motion.x;
-			sourisY = e.motion.y;
-			if(clicker == 1){
-				deplacX += -(pasSourisX - sourisX);
-				deplacY += -(pasSourisY - sourisY);
-				pasSourisX = sourisX;
-				pasSourisY = sourisY;
-				dessine();
-			}
-			break;
-		case SDL_MOUSEWHEEL :
-			if(e.motion.x>0){
-				i = calculateZoom(sourisX,sourisY,i,0);
-			}
-			else if (zoom >= 2){
-				i = calculateZoom(sourisX,sourisY,i,1);
-			}
-			break;
 		case SDL_KEYDOWN:
 			i =	clavier(e,i);
 			break;
@@ -288,11 +166,11 @@ Sint16 * extremite(float a, float d, float e, Sint16 tab[4], int signe){
 Sint16* midle(float b, float e, int signe, Sint16 tab[4]){
 	if(signe==1){
 		tab[3]= b - e;
-		tab[2]= b + e; 
+		tab[2]= b + e;
 	}
 	else{
 		tab[3]= b + e;
-		tab[2]= b - e; 
+		tab[2]= b - e;
 	}
 	return tab;
 }
@@ -333,7 +211,7 @@ void highway(){
 	if( coeff> 0){
 	extremite(a->x,fx*epaisseur/2,gx*epaisseur/2,t,1);
 	extremite(a->y,fy*epaisseur/2,gy*epaisseur/2,s,1);
-}	
+}
 else{
 	extremite(a->x,fx*epaisseur/2,gx*epaisseur/2,t,0);
 	extremite(a->y,fy*epaisseur/2,gy*epaisseur/2,s,0);
@@ -376,7 +254,7 @@ else{
 	if( coeff> 0){
 	extremite(c->x,fx*epaisseur/2,gx*epaisseur/2,t,1);
 	extremite(c->y,fy*epaisseur/2,gy*epaisseur/2,s,1);
-}	
+}
 else{
 	extremite(c->x,fx*epaisseur/2,gx*epaisseur/2,t,0);
 	extremite(c->y,fy*epaisseur/2,gy*epaisseur/2,s,0);
@@ -393,56 +271,23 @@ else{
 */
 }
 
-void dessin(){
-	Uint8 a =15;
-//	lineRGBA(renderer,(50 * zoom) + deplacX, (50 * zoom) + deplacY,(300 * zoom) + deplacX, (301*zoom) + deplacY,255,0,0,255);
-//	printf("X : %f, Y : %f\n",50*zoom+deplacX+deplacZX,50*zoom+deplacY+deplacZY);
-//	pixelRGBA(renderer, 50*zoom+deplacX+deplacZX,50*zoom+deplacY+deplacZY,255,0,0,255);
-	lineRGBA(renderer,(30 * zoom) + deplacX+ deplacZX, (80 * zoom) + deplacY+ deplacZY,(50 * zoom)+ deplacX+ deplacZX, (80 * zoom) + deplacY+ deplacZY,255,0,0,255);
-	lineRGBA(renderer,(50 * zoom) + deplacX+ deplacZX, (80 * zoom) + deplacY+ deplacZY,(50 * zoom)+ deplacX+ deplacZX, (120 * zoom) + deplacY+ deplacZY,255,0,0,255);
-	lineRGBA(renderer,(50 * zoom) + deplacX+ deplacZX, (120 * zoom) + deplacY+ deplacZY,(30 * zoom)+ deplacX+ deplacZX, (120 * zoom) + deplacY+ deplacZY,255,0,0,255);
-	lineRGBA(renderer,(30 * zoom) + deplacX+ deplacZX, (120 * zoom) + deplacY+ deplacZY,(30 * zoom)+ deplacX+ deplacZX, (80 * zoom) + deplacY+ deplacZY,255,0,0,255);
-
-	lineRGBA(renderer,(260 * zoom) + deplacX+ deplacZX, (260 * zoom) + deplacY+ deplacZY,(280 * zoom)+ deplacX+ deplacZX, (260 * zoom) + deplacY+ deplacZY,255,0,0,255);
-	lineRGBA(renderer,(280 * zoom) + deplacX+ deplacZX, (260 * zoom) + deplacY+ deplacZY,(280 * zoom)+ deplacX+ deplacZX, (280 * zoom) + deplacY+ deplacZY,255,0,0,255);
-	lineRGBA(renderer,(280 * zoom) + deplacX+ deplacZX, (280 * zoom) + deplacY+ deplacZY,(260 * zoom)+ deplacX+ deplacZX, (280 * zoom) + deplacY+ deplacZY,255,0,0,255);
-	lineRGBA(renderer,(260 * zoom) + deplacX+ deplacZX, (280 * zoom) + deplacY+ deplacZY,(260 * zoom)+ deplacX+ deplacZX, (260 * zoom) + deplacY+ deplacZY,255,0,0,255);
-	thickLineRGBA(renderer,(50 * zoom) + deplacX+ deplacZX, (60 * zoom) + deplacY+ deplacZY,(300 * zoom) + deplacX+ deplacZX, (311 * zoom) + deplacY+ deplacZY,a,255,0,0,255);
-	thickLineRGBA(renderer,(30 * zoom) + deplacX+ deplacZX, (30 * zoom) + deplacY+ deplacZY,(40 * zoom) + deplacX+ deplacZX, (20 * zoom) + deplacY+ deplacZY,a,255,0,0,255);
-	thickLineRGBA(renderer,(40 * zoom) + deplacX+ deplacZX, (20 * zoom) + deplacY+ deplacZY,(50 * zoom) + deplacX+ deplacZX, (50 * zoom) + deplacY+ deplacZY,a,255,0,0,255);
-//	lineRGBA(renderer,(50 * zoom) + deplacX, (50 * zoom) + deplacY,(windows_Width-10 * zoom) + deplacX, (windows_Height-10*zoom) + deplacY,0,0,255,255);
-//	lineRGBA(renderer,(XX * zoom) + deplacX + deplacZX, ((YY-10) * zoom) + deplacY + deplacZY,(XX * zoom) + deplacX + deplacZX, ((YY+10)*zoom) + deplacY + deplacZY,0,0,255,255);
-//	lineRGBA(renderer,((XX-10) * zoom) + deplacX + deplacZX, (YY * zoom) + deplacY + deplacZY,((XX+10) * zoom) + deplacX + deplacZX, (YY*zoom) + deplacY + deplacZY,0,0,255,255);
-}
-
 int main(int argc,char *argv[]) {
 	windows_Width = 540;
 	windows_Height  = 540;
-	
+
 	status = CONTINUE;
 	sizeWindows();
 
     if(init_SDL() == 0) {
         exit(EXIT_FAILURE);
     }
-	
+
 	colorBackground();
 	SDL_RenderClear(renderer);
 	highway();
     SDL_RenderPresent(renderer);
 
-/*	new 3
-
-	zoom = 1;
-	deplacX = 0;
-	deplacY = 0;
-	deplacZX = 0;
-	deplacZY = 0;
-	pasSourisX = 0;
-	pasSourisY = 0;
-
-	dessine();
-*/ 	evenement();
+ 	evenement();
    close_SDL();
     exit(EXIT_SUCCESS);
 
