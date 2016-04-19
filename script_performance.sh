acb8fa94a1f0a549f0e976803fc160ede2e8c618
Hafca <hafca.tirichine@gmail.com>
2016-04-19 13:36:35 +0200

    Script performance
    A script to test the performance of our program
    - Takes a directory of osm maps as argument

---
+++ script_performance.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#/usr/local/bin/bash
+
+nbFic=0
+tmpsMis=0
+
+parcoursRep(){
+  repertoire=$1
+
+  droits=`stat -c "%a" $repertoire`
+  #droits=`stat -f "%0p" $repertoire`
+  chmod u+xr $repertoire
+  cd $repertoire
+
+  for fic in `ls`
+  do
+    if [ -f $fic ]
+      then
+      echo "Fichier: $fic"
+      let nbFic++
+
+      time1=$(($(date +%s%N)/1000000))
+
+      $(../CPROJ $fic -s)
+
+      time2=$(($(date +%s%N)/1000000))
+
+      time3=$(($time2-$time1))
+      echo "temps: $time3 ms"
+
+      let tmpsMis=$(($tmpsMis+$time3))
+    else
+      if [ -d $fic ]
+        then
+        echo "   *"$fic
+        parcoursRep $fic
+      else
+        echo " $fic n'est ni un fic ni un rep, on ne fais rien"
+      fi
+    fi
+  done
+  cd ..
+  chmod $droits $repertoire
+}
+
+
+if [ $# != 1 ] || [ ! -d $1 ]
+	then
+	echo "Error: Il faut donner un nom de repertoire en argument."
+else
+	repertoire_test=$1
+	parcoursRep $repertoire_test
+	echo "Nombre de fichiers traité: "$nbFic
+  echo "Temps moyen d'execution: "$(($tmpsMis/$nbFic))" ms"
+fi
